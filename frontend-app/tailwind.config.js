f929e9500cf526e9dac6fc4415d9891fca85de9a
M-Azariel <walter.azariel.moreno@gmail.com>
2024-10-07 20:30:05 -0300

    agregado de texto, interlineado, efecto de imagen en sobre nosotros y  espaciado de renglones en los textos

--- frontend-app/tailwind.config.js
+++ frontend-app/tailwind.config.js
@@ -7,8 +7,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        'orange': '#F39C12'
+        'orange': '#F39C12',
+        'Newhite' : '#F8F9FA',
+        'Blue': '#1F485C'
       },
+
+      animation:{
+        bounceIn: 'bounceIn 1s ease-in-out',
+      },
+
+      keyframes:{
+        bounceIn:{
+          '0%, 20%, 50%, 80%, 100%': { transform: 'translateY(0)' },
+          '40%': { transform: 'translateY(-30px)' },
+          '60%': { transform: 'translateY(-15px)' },
+        }
+      }
+      
     },
   },
   plugins: [],
