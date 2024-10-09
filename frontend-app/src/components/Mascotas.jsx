1cbcbf0dbcce756471aeffd92ed461afeb12f65d
M-Azariel <walter.azariel.moreno@gmail.com>
2024-10-08 21:15:05 -0300

    mejora del padding del texto en header

--- frontend-app/src/components/Mascotas.jsx
+++ frontend-app/src/components/Mascotas.jsx
@@ -5,7 +5,7 @@ import '../index.css'
 export const Mascotas = ({ pets }) => {
   return (
     <div className="flex flex-col pt-[70px]  justify-center items-center w-full ">
-      <h1 className="text-4xl color-azul  font-bold px-20 mb-[76px] text-center">Encuentra tu mascota, tenemos amigos que buscan cambiar su vida y la tuya</h1>
+      <h1 className="text-4xl color-azul  font-bold px-20 mb-[73px] text-center">Encuentra tu mascota, tenemos amigos que buscan cambiar su vida y la tuya</h1>
       {
         pets.map((pet , index) => (
           
