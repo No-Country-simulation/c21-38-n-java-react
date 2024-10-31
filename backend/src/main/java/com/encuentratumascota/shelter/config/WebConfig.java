0ad1a886f98fab79d73f4dfb34cda47fd91727d3
Frankma0117 <franmacagu@gmail.com>
2024-10-30 23:50:34 -0500

    Corregir validacioens de errores en users

--- backend/src/main/java/com/encuentratumascota/shelter/config/WebConfig.java
+++ backend/src/main/java/com/encuentratumascota/shelter/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(false);
+                .allowCredentials(true);
     }
 }
